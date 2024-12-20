b7400677ec65e19c9b1aa679df87e00e0e5096cc
an.nguyen <an.nguyen@dareplay.io>
2024-12-20 15:14:30 +0700

    update version sdk

--- index_sponsor.ts
+++ index_sponsor.ts
@@ -1,9 +1,5 @@
 import { BigNumber } from "ethers";
-import {
-  Hex,
-  encodeFunctionData,
-  parseAbi,
-} from "viem";
+import { Hex, encodeFunctionData, parseAbi } from "viem";
 import { createSmartAccountClient, sendUserOps } from "@derachain/aa-sdk";
 
 import dotenv from "dotenv";
@@ -13,9 +9,12 @@ export const createAccountAndMintNft = async () => {
   // 1. Create Smart Account from signer (private key)
   const isPaymaster = true;
   const index = 0; // change index to create new SA
-  const smartAccount = await createSmartAccountClient(index, `0x${process.env.PRIVATE_KEY!}`);
-  console.log('SA address', await smartAccount.getAccountAddress());
-  
+  const smartAccount = await createSmartAccountClient(
+    index,
+    `0x${process.env.PRIVATE_KEY!}`
+  );
+  console.log("SA address", await smartAccount.getAccountAddress());
+
   // ------ 2. Mint NFT to Smart Account
   try {
     // create sponsored gasless transaction
@@ -24,7 +23,10 @@ export const createAccountAndMintNft = async () => {
     const callData = encodeFunctionData({
       abi: parsedAbi,
       functionName: "mint",
-      args: [await smartAccount.getAccountAddress() as Hex, BigNumber.from(0).toBigInt()],
+      args: [
+        (await smartAccount.getAccountAddress()) as Hex,
+        BigNumber.from(0).toBigInt(),
+      ],
     });
 
     const transaction = {
@@ -32,18 +34,22 @@ export const createAccountAndMintNft = async () => {
       data: callData,
     };
 
-    const userOpResponse = await sendUserOps(smartAccount, [transaction], isPaymaster);
+    const userOpResponse = await sendUserOps(
+      smartAccount,
+      [transaction],
+      isPaymaster
+    );
     console.log(`userOpHash: ${userOpResponse.userOpHash}`);
 
-    const {transactionHash} = await userOpResponse.waitForTxHash();
+    const { transactionHash } = await userOpResponse.waitForTxHash();
     console.log(`transactionHash:  ${transactionHash}`);
 
     const userOpReceipt = await userOpResponse.wait();
     if (userOpReceipt.success == "true") {
       console.log("UserOp receipt", userOpReceipt);
       console.log("Transaction receipt", userOpReceipt.receipt);
-    } else{
-      console.log("UserOp failed", userOpReceipt)
+    } else {
+      console.log("UserOp failed", userOpReceipt);
     }
   } catch (error: unknown) {
     if (error instanceof Error) {
